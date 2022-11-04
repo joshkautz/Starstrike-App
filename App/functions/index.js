3f3e48c5ad974bca16499a9ee27ab62a75cd559c
Josh Kautz <josh@joshkautz.com>
2022-11-04 16:06:04 -0500

    Creating my first Firebase Function.

--- App/functions/index.js
+++ App/functions/index.js
@@ -1,9 +1,42 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Take the GitHub Payload passed to this HTTP endpoint and insert it into 
+// Firestore under the path /users/:documentId
+exports.addUser = functions.https.onRequest(async (request, response) => {
+    // Log the original request.
+    functions.logger.log(request);
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    // const writeResult = await admin.firestore().collection('users').add(request);
+    // Send back a message that we've successfully written the message.
+    response.json({'message': 'tahnk you'});
+});
+
+// Listens for new documents added to /users/:documentId and performs actions
+// with the GitHub Starring REST API.
+// exports.createRepo = functions.firestore.document('/users/{documentId}')
+//     .onCreate((snap, context) => {
+//         functions.logger.log(snap);
+//         functions.logger.log(snap.data());
+
+//         // Grab the current value of what was written to Firestore.
+//         // const data = snap.data();
+
+//         // Access the parameter `{documentId}` with `context.params`
+//         // functions.logger.log('Accessing GitHub Starring REST API.', context.params.documentId, data);
+
+//         // Create Repo
+//         // Star other Repos.
+//         // Have other users star this repo.
+
+//         // You must return a Promise when performing asynchronous tasks inside a Functions such as
+//         // writing to Firestore.
+//         // Setting an 'uppercase' field in Firestore document returns a Promise.
+//         // return snap.ref.set({ uppercase }, { merge: true });
+
+//         return;
+//     });
