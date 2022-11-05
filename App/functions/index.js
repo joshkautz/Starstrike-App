f612de21c4bcc58eb9d02a228fdd4fcdd404b922
Josh Kautz <josh@joshkautz.com>
2022-11-04 23:07:50 -0500

    Storing the username of the authenticated users because we'll need it for using the GitHub Starred REST API.

--- App/functions/index.js
+++ App/functions/index.js
@@ -10,48 +10,65 @@ if (admin.apps.length === 0) {
     admin.initializeApp();
 }
 
-
-// Store the Authorization Code in Firestore, and use it to generate an Access Token. Redirect to Starstruck website.
+// Use the Authorization Code to generate an Access Token. Store the Access Token in Firestore. Redirect to Starstruck website.
 exports.authorization = functions.https.onRequest(async (request, response) => {
     try {
         // Request an Access Token from GitHub using the Axios library.
-        const response = await axios({
+        const accessTokenRequest = await axios({
             method: 'POST',
             url: `https://github.com/login/oauth/access_token?client_id=9637b925c8fc340a9c4c&client_secret=${process.env.CLIENT_SECRET}&code=${request.query.code}`,
             headers: { 'Accept': 'application/json' }
         });
 
-        if (response.status != 200)
-            throw 'Error retrieving Access Token';
+        functions.logger.info(createRepoRequest.data);
 
         // Store the Access Token in Firestore using the Firebase Admin SDK.
-        const writeResult = await admin.firestore().collection('authorizations').add(response.data);
+        await admin.firestore().collection('authorizations').add(accessTokenRequest.data);
 
         // Redirect to Starstruck website success screen!
-        response.redirect('https://joshkautz.github.io/Starstruck-GitHub-App/');
+        response.redirect('https://joshkautz.github.io/Starstruck-GitHub-App?authorization=succeeded');
     } catch (error) {
         // Log the error.
         functions.logger.error(error);
 
         // Redirect to Starstruck website error screen!
-        response.redirect('https://www.google.com/');
+        response.redirect('https://joshkautz.github.io/Starstruck-GitHub-App?authorization=failed');
     }
 });
 
 // Triggered upon creation of documents in the `/authorizations` Firestore collection.
 // Creates a new repository for the GitHub user via the GitHub REST API.
 exports.createRepo = functions.firestore.document('/authorizations/{documentId}')
-    .onCreate((snap, context) => {
+    .onCreate(async (snap, context) => {
         try {
-            // Grab the current value of what was written to Firestore.
+            // Get the current value of what was written to Firestore.
             const data = snap.data();
 
+            functions.logger.info(context.params.documentId, data);
+
             // Create a new repository using the GitHub API.
-            return axios({
+            const createRepoRequest = await axios({
                 method: 'POST',
                 url: 'https://api.github.com/user/repos',
                 headers: { 'Authorization': `Bearer ${data.access_token}` },
-                json: { "name": `Starstruck-${context.params.documentId}` }
+                data: { "name": `Starstruck-${context.params.documentId}` }
+            });
+
+            functions.logger.info(createRepoRequest.data);
+
+            // Get the authenticated user using the GitHub API.
+            const getAuthenticatedUserRequest = await axios({
+                method: 'GET',
+                url: 'https://api.github.com/user',
+                headers: { 'Authorization': `Bearer ${data.access_token}` }
+            });
+
+            functions.logger.info(getAuthenticatedUserRequest.data);
+
+            // Add the username of the authenticated user to the Firestore document using the Firebase Admin SDK.
+            snap.ref.update({
+                user: getAuthenticatedUserRequest.data.login,
+                documentId: context.params.documentId
             });
         } catch (error) {
             // Log the error.
@@ -59,26 +76,19 @@ exports.createRepo = functions.firestore.document('/authorizations/{documentId}'
         }
     });
 
-// Receive GitHub Hooks to monitor if users uninstall the GitHub App or Revoke Authorization.
-// exports.hook = functions.https.onRequest(async (request, response) => {
-//     // Log the original request.
-//     functions.logger.log(request);
 
-//     // TODO: Remove user from Firestore. (Delete their repo? Have users unstar their repo?)
+// exports.starRepos = functions.pubsub.schedule('every 1 hours').onRun(async (context) => {
+//     // Retrieve all Access Tokens from Firestore.
+//     const querySnapshot = await admin.firestore().collection('authorizations').get();
 
-//     // Send back a message that we've successfully written the message.
-//     response.json(request);
-// });
+//     querySnapshot.forEach((document) => {
+//         // doc.data() is never undefined for query doc snapshots
+//         console.log(doc.id, " => ", doc.data());
 
-// Store the GitHub passed to this HTTP endpoint and insert it into 
-// Firestore under the path /installations/:documentId
-// exports.install = functions.https.onRequest(async (request, response) => {
-//     // Log the original request.
-//     functions.logger.log(request.query);
-
-//     // Push the new message into Firestore using the Firebase Admin SDK.
-//     const writeResult = await admin.firestore().collection('installations').add(request.query);
-
-//     // Send back a message that we've successfully written the message.
-//     response.json(writeResult);
+//         // Authenticate as user, and Star each repository  (which is available in the results from the initial query)
+//     });
+    
+    
+//     console.log('This will be run every 5 minutes!');
+//     return null;
 // });
